b07caa8fe7b60bfbb6581514f820b5883aa1a3bb
Owen Pattison <opattison@neworbit.co.uk>
2019-04-12 11:19:16 +0100

    remove build and ignore

--- build/lib/widget.d.ts
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-export interface SampleWidgetProps {
-    name: string;
-    age: number;
-}
-export declare class SampleWidget extends React.Component<SampleWidgetProps, {}> {
-    render(): JSX.Element;
-}
