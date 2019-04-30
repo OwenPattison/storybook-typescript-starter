2dcbe2aa213115aba92878c5179ca4c9ae65847b
Owen Pattison <opattison@neworbit.co.uk>
2019-04-30 16:56:41 +0100

    Update widget.tsx

--- src/widget.tsx
+++ src/widget.tsx
@@ -1,16 +1,11 @@
 import * as React from "react";
 
-type PropTypes = {
-    name: string;
-    age: number;
-}
-
 interface SampleWidgetProps {
     name: string;
     age: number;
 }
 
-const SampleWidget: React.FunctionComponent<SampleWidgetProps> = ({ name, age }: PropTypes ) => (
+const SampleWidget: React.FunctionComponent<SampleWidgetProps> = ({ name, age }) => (
     <div>
         <h1>Hello {name} {age}</h1>
     </div>
